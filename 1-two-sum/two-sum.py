9d30eee8df5e0e4163c26b8d625baf114a5f1edd
Kingsolima <omr.m.soliman@gmail.com>
2026-07-13 12:07:47 -0400

    Time: 1746 ms (16.45%) | Memory: 19.8 MB (81.97%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # We will use a nested for loop to solve this. By having the first for loop to go through one by one in nums and each time going through the nested for loop to see if it adds up to the target
-
-        for left in range(len(nums)):
-            for right in range(left+1, len(nums)):
-                if nums[left] + nums[right] == target:
-                    return [left, right]
-        return False
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return (i,j)
+        
