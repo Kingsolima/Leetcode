19165c2fb3082e3278702bbc3d1db2050689c4d3
Kingsolima <omr.m.soliman@gmail.com>
2026-07-25 12:11:47 -0400

    Time: 18 ms (19.52%) | Memory: 20.2 MB (18.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,16 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        s_word = list(s)
-        t_word = list(t)
-        s_word.sort()
-        t_word.sort()
-
-        return s_word == t_word
+        return sorted(s)==sorted(t)
