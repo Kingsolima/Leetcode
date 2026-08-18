34f95dbf3e165a551aad8ae50688d4df7877c0c7
Kingsolima <omr.m.soliman@gmail.com>
2026-08-17 20:55:43 -0400

    Time: 64 ms (5.08%) | Memory: 29.4 MB (99.80%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,8 +1,32 @@
 class Solution:
+    # Brute force:
+    """
+    input: int array called nums
+    output: return True if any value appears at least twice, else return false
+
+    1. for every element
+        2. put it in seen
+        3. for every element after that one
+            4. if element in seen
+                6. return true
+    return false
+
+    time: O(n^2)
+    space: O(n)
+    """
+
+    # Optimal:
+    """
+    1. sort it
+    3. for loop (1,end)
+        4. if nums[i-1]==nums[i]:
+            return true
+    return false
+    """
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums=sorted(nums)
-        for i in range(1, len(nums)):
-            if nums[i-1]==nums[i]:
+        new_nums = sorted(nums)
+        for i in range(1, len(new_nums)):
+            if new_nums[i-1] == new_nums[i]:
                 return True
         return False
         
