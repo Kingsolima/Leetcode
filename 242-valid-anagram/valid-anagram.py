1a621da085e8837b767a29712fd5ad150d0a1614
Kingsolima <omr.m.soliman@gmail.com>
2026-08-17 21:47:25 -0400

    Time: 11 ms (77.30%) | Memory: 19.3 MB (94.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,56 @@
 class Solution:
-    #countS
-    #countT
-    #
+    # brute:
+    """
+    new_s = sorted(s)
+    new_t = sorted(t)
+
+    if len(s)!=len(t):
+        return False
+    return new_s == new_t
+
+    time: o(mlogm + nlogn)
+    space: o(m+n)
+    """
+
+    # OPTIMAL:
+    """
+    new_s = {}
+    new_t = {}
+
+    if len(s)!=len(t):
+        return False
+    
+    for i in range(len(s)):
+        if s[i] in new_s:
+            new_s[s[i]] += 1
+        else:
+            new_s[s[i]] = 1
+        if t[i] in new_t:
+            new_t[t[i]] += 1
+        else:
+            new_t[t[i]] = 1
+
+    return new_t==new_s
+
+    time: O(n)
+    space: O(m+n)
+        
+    """
     def isAnagram(self, s: str, t: str) -> bool:
+        new_s = {}
+        new_t = {}
+
         if len(s)!=len(t):
             return False
-        countS = {}
-        countT = {}
+        
         for i in range(len(s)):
-            countT[t[i]]=1+countT.get(t[i],0)
-            countS[s[i]]=1+countS.get(s[i],0)
-        return countT==countS
+            if s[i] in new_s:
+                new_s[s[i]] += 1
+            else:
+                new_s[s[i]] = 1
+            if t[i] in new_t:
+                new_t[t[i]] += 1
+            else:
+                new_t[t[i]] = 1
+
+        return new_t==new_s
