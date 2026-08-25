6c8f96be42548167dfc263f80c350cddfb6e41d4
Kingsolima <omr.m.soliman@gmail.com>
2026-08-24 20:20:44 -0400

    Time: 35 ms (79.42%) | Memory: 28.6 MB (41.60%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -6,6 +6,9 @@ class Solution:
     output = profit or 0
 
     method: use a nested for loop that has two pointers and has a max variable so that it checks for every difference calculated it goes through an if statement to determine if that is the max profit, else 0.
+
+    time: o(n)
+    space: o(1)
     """
     def maxProfit(self, prices: List[int]) -> int:
         max = 0
