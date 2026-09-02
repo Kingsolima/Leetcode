5e0df75d4dd77097d73044395f26a719eec5df28
Kingsolima <omr.m.soliman@gmail.com>
2026-09-02 19:27:21 -0400

    Time: 19 ms (42.25%) | Memory: 37.4 MB (7.70%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,46 +1,26 @@
 class Solution:
     # Brute force:
     """
-    input: int array called nums
-    output: return True if any value appears at least twice, else return false
+    seen={}
+    for loop:
+        if nums[i] in seen:
+            return true
+        seen[nums[i]] = 1
+    
+    return False
 
-    1. for every element
-        2. put it in seen
-        3. for every element after that one
-            4. if element in seen
-                6. return true
-    return false
-
-    time: O(n^2)
-    space: O(n)
+    Time: O(n)
+    Space: O(n)
     """
 
     # Optimal:
     """
-    1. sort it
-    3. for loop (1,end)
-        4. if nums[i-1]==nums[i]:
-            return true
-    return false
-    time: O(nlogn)
-    speed: O(n)
-    """
-    # MOST OPTIMAL
-    """
-    seen = ()
-    for i in range:
-        if nums[i] in seen:
-            return true
-        seend.add(nums[i])
-
-    return false
-    time: 
     """
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen=set()
+        seen={}
         for i in range(len(nums)):
             if nums[i] in seen:
                 return True
-            seen.add(nums[i])
+            seen[nums[i]] = 1
         return False
         
